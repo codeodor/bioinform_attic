927e296c2dea894c5e8b20a929eeead96f3ce986
Sammy Larbi <sam@codeodor.com>
2008-10-13 20:33:28 -0500

    cleaned up sequence and nucleotide classes. added file format handler.

--- sequence.rb
+++ sequence.rb
@@ -1,11 +1,18 @@
+require 'file_formats'
+
 module Nucleotide
   def complement
     result = ""
     self.each do |nucleotide|
-      result << 't' if nucleotide == 'a'
-      result << 'a' if nucleotide == 't'
-      result << 'c' if nucleotide == 'g'
-      result << 'g' if nucleotide == 'c' 
+      result << 'T' if nucleotide == 'a'
+      result << 'A' if nucleotide == 't'
+      result << 'C' if nucleotide == 'g'
+      result << 'G' if nucleotide == 'c'
+      
+      result << 'T' if nucleotide == 'A'
+      result << 'A' if nucleotide == 'T'
+      result << 'C' if nucleotide == 'C'
+      result << 'G' if nucleotide == 'G' 
     end
     return result
   end
@@ -15,20 +22,37 @@ class String
   include Nucleotide
 end
 
-module Sequence
+class Sequence
+  include Enumerable
+  
+  def initialize(*args)
+    if args.length == 1
+      @name = ""
+      if args[0].class == Array 
+        @sequence = args[0]
+      elsif args[0].class == String
+        @sequence = args[0].split
+      else
+        throw Exception.new "Sequence should be initialized from a string or array if only 1 argument is used"
+      end
+    elsif args.length == 2
+      filename = args[0]
+      filetype = args[1]
+      #assuming filetype == 'old' for time being
+      @name, @sequence = FileFormats.old_to_name_and_string(filename)
+      @sequence = @sequence.split
+    end
+  end
+
   def reverse_complement
-    result = self.empty
-    self.reverse.each do |nucleotide|
+    result = []
+    @sequence.reverse.each do |nucleotide|
       result << nucleotide.complement
     end
     return result
   end
+  
+  def to_s
+    @sequence.join
+  end
 end
-
-module Enumerable
-  include Sequence
-end
-
-s = ['a','c','t','g']
-#s = "actg"
-puts s.reverse_complement.join
