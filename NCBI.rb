2b5a92784ea65dc7b997c46fc1f6a4c109f31da2
Sammy Larbi <sam@codeodor.com>
2009-01-21 21:00:59 -0600

    Added blastx capability to NCBI class

--- NCBI.rb
+++ NCBI.rb
@@ -12,4 +12,43 @@ class NCBI
     as_fasta = http_response[start_at..end_at]
     return as_fasta
   end
+
+  def self.blastx(sequence, database="nr", wait=3)
+    http_response = Net::HTTP.get(URI.parse("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Put&QUERY=#{sequence}&DATABASE=#{database}&PROGRAM=blastx"))
+    params = blast_results_params(http_response)
+    request_id = params[:RID]
+    estimated_time_of_execution = params[:RTOE].to_i
+    
+    until(blast_results_ready? http_response)
+      sleep estimated_time_of_execution
+      estimated_time_of_execution = wait
+      http_response = Net::HTTP.get(URI.parse("http://www.ncbi.nlm.nih.gov/blast/Blast.cgi?RID=#{request_id}&CMD=Get"))
+    end
+    return http_response 
+  end
+  
+  
+  
+  private
+  def self.blast_results_params(http_response)
+    start_marker = 'QBlastInfoBegin'
+    finish_marker = 'QBlastInfoEnd'
+    start = http_response.index(start_marker) + start_marker.length
+    finish = http_response.index(finish_marker) - 1 
+    
+    param_array = http_response[start..finish].gsub(/\t/,'').gsub(' ','').split
+    params = {}
+    param_array.each do |x|
+      name = x.split('=')[0]
+      value = x.split('=')[1]
+      params[name.to_sym] = value
+    end
+    return params
+  end
+  
+  def self.blast_results_ready?(http_response)
+    params = blast_results_params(http_response)
+    return params[:Status] == 'READY'
+  end
+
 end
