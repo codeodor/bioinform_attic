b42dbf2c88ee39fe292a6ca49e6c997e47ef40b1
Sammy Larbi <sam@codeodor.com>
2009-08-29 16:37:53 -0500

    fixed NCBI get by gi to work w/ entrez changes on their end

--- NCBI.rb
+++ NCBI.rb
@@ -1,16 +1,8 @@
 class NCBI
   require 'net/http'
   def self.get_fasta_sequence_of_gi(gi, from='begin', to='end')
-    http_response = Net::HTTP.get 'www.ncbi.nlm.nih.gov', "/entrez/viewer.fcgi?db=nucleotide&list_uids=#{gi}&dopt=fasta&from=#{from}&to=#{to}"
-    
-    start_search_string = '<div class=\'recordbody\'>'
-    start_at = http_response.index(start_search_string) + start_search_string.length
-    
-    end_search_string = '</div>'
-    end_at = http_response.index(end_search_string, start_at) - 1
-    
-    as_fasta = http_response[start_at..end_at]
-    return as_fasta
+    fasta = Net::HTTP.get_response('eutils.ncbi.nlm.nih.gov', "/entrez/eutils/efetch.fcgi?db=nucleotide&id=#{gi}&rettype=fasta&seq_start=#{from}&seq_stop=#{to}")
+    return fasta.read_body
   end
 
   def self.generic_blast(program, sequence, database="nr", wait=3)
